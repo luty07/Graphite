98cd2886584b7676991b67326452aac38e2f3d68
Adam <adamgerhant@gmail.com>
2025-07-12 01:53:41 -0700

    Simplify compilation

--- editor/src/messages/tool/tool_messages/path_tool.rs
+++ editor/src/messages/tool/tool_messages/path_tool.rs
@@ -330,21 +330,21 @@ impl<'a> MessageHandler<ToolMessage, &mut ToolActionHandlerData<'a>> for PathToo
 						responses.add(ToolMessage::UpdateHints);
 						let pivot_gizmo = self.tool_data.pivot_gizmo();
 						responses.add(TransformLayerMessage::SetPivotGizmo { pivot_gizmo });
-						responses.add(NodeGraphMessage::RunDocumentGraph);
+						responses.add(PortfolioMessage::CompileActiveDocument);
 						self.send_layout(responses, LayoutTarget::ToolOptions);
 					}
 				}
 				PathOptionsUpdate::TogglePivotGizmoType(state) => {
 					self.tool_data.pivot_gizmo.state.disabled = !state;
 					responses.add(ToolMessage::UpdateHints);
-					responses.add(NodeGraphMessage::RunDocumentGraph);
+					responses.add(PortfolioMessage::CompileActiveDocument);
 					self.send_layout(responses, LayoutTarget::ToolOptions);
 				}
 
 				PathOptionsUpdate::TogglePivotPinned => {
 					self.tool_data.pivot_gizmo.pivot.pinned = !self.tool_data.pivot_gizmo.pivot.pinned;
 					responses.add(ToolMessage::UpdateHints);
-					responses.add(NodeGraphMessage::RunDocumentGraph);
+					responses.add(PortfolioMessage::CompileActiveDocument);
 					self.send_layout(responses, LayoutTarget::ToolOptions);
 				}
 			},
@@ -2283,7 +2283,7 @@ impl Fsm for PathToolFsmState {
 				tool_data.pivot_gizmo.pivot.set_normalized_position(position.unwrap());
 				let pivot_gizmo = tool_data.pivot_gizmo();
 				responses.add(TransformLayerMessage::SetPivotGizmo { pivot_gizmo });
-				responses.add(NodeGraphMessage::RunDocumentGraph);
+				responses.add(PortfolioMessage::CompileActiveDocument);
 
 				self
 			}
